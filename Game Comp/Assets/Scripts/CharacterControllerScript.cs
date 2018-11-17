6a369e22f23c5ee77d891be119451617c6d93f54
Kye Yeung <yeung.kye@gmail.com>
2018-11-17 15:21:24 +0000

    - the animation transitions on the ninja have no exit time now so sharper - jump blend animation was on the wrong param so didn't animate now fixed - copied a custom physics 2d controller from another tutorial just to get a comparison, probably will delete most of it later.

--- Game Comp/Assets/Scripts/CharacterControllerScript.cs
+++ Game Comp/Assets/Scripts/CharacterControllerScript.cs
@@ -5,80 +5,68 @@ using UnityEngine;
 public class CharacterControllerScript : MonoBehaviour {
 
 	public float maxSpeed = 10f;
-	bool facingRight = true;
-    bool grounded = false;
-    public Transform groundCheck;
-    float groundRadius = 0.2f;
-    public LayerMask whatIsGround;
-    Rigidbody2D rigidbody2D;
     public float jumpForce = 700f;
-    bool usedDoubleJump = false;
+    public Transform groundCheck;// an invisible game object that is placed at the character sprite's feet
+    public LayerMask whatIsGround;// in the IDE set this to be every layer that is not in the "player" layer
+    
+    bool facingRight = true;
+    bool grounded = false;
+    float groundRadius = 0.2f;
+    int jumpCount = 0;
+    Rigidbody2D rBody2D;
     Animator animator;
+    SpriteRenderer spriteRenderer;
 
-	void Start () {
-	
+
+    void Start() {
+     
+        rBody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-
-    // trying to find the values for setting the jump animation
-    bool debug = true;
-    float lowestVertical = 0;
-    float highestVertcal = 0;
-
-
-	void FixedUpdate () {
-
+    void FixedUpdate () {
 
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
-
         if(grounded){
-            usedDoubleJump = false;
+            jumpCount = 0;
         }
-
-
-        animator.SetBool("Ground", grounded);
-
-        // how fast are we moving up or down
-        animator.SetFloat("vSpeed", rigidbody2D.velocity.y);
 
         float move = Input.GetAxis("Horizontal");
+
+        MoveCharacter(move);
+        AnimateChacter(move);
+    }
+
+    void MoveCharacter(float moveX){
+      
+        rBody2D.velocity = new Vector2(moveX * maxSpeed, rBody2D.velocity.y);
+    }
+
+    void AnimateChacter(float move){
+    
+        animator.SetBool("Ground", grounded);
+        animator.SetFloat("vSpeed", rBody2D.velocity.y);
         animator.SetFloat("Speed", Mathf.Abs(move));
 
-        rigidbody2D.velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
-       
-        if(NeedsFlip(move)){
-			Flip();
-		}
-	}
-
-    private void Update() {
-
-
-        // just doing this to get some values.
-        if (debug){
-            float currentV= rigidbody2D.velocity.y;
-            if(currentV> highestVertcal){
-                highestVertcal = currentV;
-            }
-            if (currentV < lowestVertical){
-                lowestVertical = currentV;
-            }
-
-            Debug.Log("highest v=" + highestVertcal +" lowest v=" + lowestVertical);
-        }
-
-        if ((grounded || !usedDoubleJump) && Input.GetKeyDown(KeyCode.Space)){
-            animator.SetBool("Ground", false);
-            rigidbody2D.AddForce(new Vector2(0, jumpForce));
-        
-            if(!usedDoubleJump && !grounded){
-                usedDoubleJump = true;
-            }
+        if (NeedsFlip(move)) {
+            Flip();
         }
     }
 
+    // TODO don't really understand why the tutorial had some code in FixedUpdate and some in Update... need to investigate
+    void Update() {
+
+        // if pressing the jump button and you're grounded OR you haven't doubled jumped...
+        if ( jumpCount < 1 && Input.GetButtonDown("Jump")) {
+
+            // do the jump
+            rBody2D.AddForce(new Vector2(0, jumpForce));
+            jumpCount++;
+        }
+
+        AnimateChacter(rBody2D.velocity.x);
+    }
 
     bool NeedsFlip(float move){
 
@@ -86,10 +74,8 @@ public class CharacterControllerScript : MonoBehaviour {
     }
 
 	void Flip(){
-	
+
         facingRight = !facingRight;
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
+        spriteRenderer.flipX =  !spriteRenderer.flipX;
 	}
 }

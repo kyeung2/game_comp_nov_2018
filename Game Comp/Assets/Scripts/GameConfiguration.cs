38a0d4ca5422ffa2959a127a32c57883bcb1fa22
Kye Yeung <yeung.kye@gmail.com>
2018-11-25 12:58:57 +0000

    - adding Vito's menus - adding Carl's sounds

---
+++ Game Comp/Assets/Scripts/GameConfiguration.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConfiguration  {
+    // How many rounds to play
+    public int rounds = 10;
+}
